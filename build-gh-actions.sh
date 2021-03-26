95cd92a04da4abf217d4f0bdf768542db3f36dc9
Quirin <quirin.ecker@bajupa.com>
2021-03-26 19:05:36 +0100

    adjusted buld script for the github actions

--- build-gh-actions.sh
+++ build-gh-actions.sh
@@ -1,4 +1,1 @@
-docker run --rm \
-           -v ${PWD}:/documents \
-           --env-file ./.env \
-           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/build-html-docker-inside-docker.sh"
+./build-html-docker-inside-docker.sh
